dc7fa74f1d8ac30ed46287c936c292c432f91315
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2025-04-09 21:07:43 +0300

    added more routes, ternary operators, page404

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } 
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobsPage from "./pages/JobsPage";
+import Error404 from "./pages/Error404";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<MainLayout></MainLayout>}>
     <Route index element={<HomePage></HomePage>}></Route>
     <Route path='/jobs' element={ <JobsPage></JobsPage> }></Route>
+    <Route path='*' element={ <Error404></Error404> }></Route>
+
   </Route>
   
 )
