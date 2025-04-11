a2cb5352b33a1ba2a5fd7e6777f7a196614fcae6
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2025-04-11 23:49:39 +0300

    added conditional visibility

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,67 +1,70 @@
-import React from 'react'
-import logo from '../assets/images/logo.png';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import logo from "../assets/images/logo.png";
+import { NavLink } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
+import {toast} from 'react-toastify';
 
 const Navbar = () => {
-
-  const linkClass =  ({isActive}) => isActive ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' : 'text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2' ;
+  const { isAuthenticated } = useContext(AuthContext);
+  
+  const linkClass = ({ isActive }) =>
+    isActive
+      ? "bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+      : "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
   return (
     <>
-       <nav className="bg-indigo-700 border-b border-indigo-500">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="flex h-20 items-center justify-between">
-          <div
-            className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
-          >
-            {/* <!-- Logo --> */}
-            <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
-              <img
-                className="h-15 w-auto"
-                src={ logo }
-                alt="React Jobs"
-              />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2"
-                >React Jobs</span>
-            </NavLink>
-            <div className="md:ml-auto">
-              <div className="flex space-x-2">
-                <NavLink
-                  to="/"
-                  className={linkClass}
-                  >Home</NavLink>
+      <nav className="bg-indigo-700 border-b border-indigo-500">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="flex h-20 items-center justify-between">
+            <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+              {/* <!-- Logo --> */}
+              <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
+                <img className="h-15 w-auto" src={logo} alt="React Jobs" />
+                <span className="hidden md:block text-white text-2xl font-bold ml-2">
+                  React Jobs
+                </span>
+              </NavLink>
+              <div className="md:ml-auto">
+                <div className="flex space-x-2">
+                  <NavLink to="/" className={linkClass}>
+                    Home
+                  </NavLink>
 
-                <NavLink
-                  to="/jobs"
-                  className={linkClass}
-                  >Jobs</NavLink>
+                  <NavLink to="/jobs" className={linkClass}>
+                    Jobs
+                  </NavLink>
 
-                <NavLink
-                  to="/login"
-                  className={linkClass}
-                  >Login</NavLink>
+                  <NavLink to="/login" className={linkClass}>
+                    Login
+                  </NavLink>
 
-                <NavLink
-                  to="/register"
-                  className={linkClass}
-                  >Register</NavLink>
+                  <NavLink to="/register" className={linkClass}>
+                    Register
+                  </NavLink>
 
-                <NavLink
-                  to="/add-job"
-                  className={linkClass}
-                  >Add Job</NavLink>
-
-                <NavLink
-                  to="/logout"
-                  className={linkClass}
-                  >Logout</NavLink>
+                  {isAuthenticated ? (
+                    <NavLink to="/add-job" className={linkClass}>
+                      Add Job
+                    </NavLink>
+                  ) : null}
+                  {isAuthenticated ? (
+                    <NavLink to="/" className={linkClass} onClick={() => {
+                      localStorage.removeItem('accessToken');
+                      
+                      toast.success('Successfully Logged out!')
+                    }}>
+                      Logout
+                    </NavLink>
+                  ) : null}
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </nav> 
+      </nav>
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
